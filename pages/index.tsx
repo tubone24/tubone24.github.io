2848f3ba877eae6d83a90668ef4239ae659e13d0
tubone24 <tubo.yyyuuu@gmail.com>
2021-08-08 11:34:47 +0900

    build

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,14 @@
 import { h, Fragment } from "../deps.ts";
 import type { PageProps, GetStaticData } from "../deps.ts";
 import Header from "../components/molecules/header.tsx";
+import Timeline from "../components/molecules/timeline.tsx";
 
 function IndexPage() {
   return (
     <div class="mx-auto max-w-screen-md px-6 sm:px-6 md:px-8">
       <Header />
       <main>
-        <div class="flex flex-wrap items-stretch w-full relative">
-
-        </div>
+        <Timeline />
       </main>
     </div>
   );
